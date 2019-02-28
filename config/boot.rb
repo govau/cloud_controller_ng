dbced7008de405c258ef0c11aa4810a4e2ed025a
Adam Eijdenberg <adam.eijdenberg@digital.gov.au>
2019-02-28 13:04:55 +1100

    Allowing specification of our own env var for TMPDIR

--- config/boot.rb
+++ config/boot.rb
@@ -5,4 +5,10 @@ $LOAD_PATH.unshift(File.expand_path('../app', __dir__))
 $LOAD_PATH.unshift(File.expand_path('../middleware', __dir__))
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
-require 'bootscale/rails'
+
+# instead of using require 'bootscale/rails' per the docs, we'll
+# instead do what it does, so that we can specify a custom TMPDIR
+require 'bootscale'
+Bootscale.setup(cache_directory: ENV['BOOTSCALE_TMP'] || 'tmp/bootscale')
+require 'bootscale/active_support'
+Bootscale::ActiveSupport.setup
